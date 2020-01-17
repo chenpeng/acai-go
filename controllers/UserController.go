8f0076583d49f421db36a65fe1033b8301fd4353
Chen Peng <chenpeng.net@gmail.com>
2020-01-17 20:07:06 +0800

    add UserController.login

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,7 +1,19 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+)
 
 type UserController struct {
 	beego.Controller
+}
+
+// @Title CreateUser
+// @Description create users
+// @Param	body		body 	models.User	true		"body for user content"
+// @Success 200 {int} models.User.Id
+// @Failure 403 body is empty
+// @router /login [post]
+func (mrc *UserController) login() {
+
 }
