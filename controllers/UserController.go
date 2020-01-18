bedda338ea72450242b04b1e54aa802bfe548abf
Chen Peng <chenpeng.net@gmail.com>
2020-01-18 17:14:57 +0800

    add UserController.signIn signOut signUp

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -8,12 +8,32 @@ type UserController struct {
 	beego.Controller
 }
 
-// @Title CreateUser
-// @Description create users
+// @Title 登录
+// @Description 用户登录
 // @Param	body		body 	models.User	true		"body for user content"
 // @Success 200 {int} models.User.Id
 // @Failure 403 body is empty
-// @router /login [post]
-func (mrc *UserController) login() {
+// @router /signIn [post]
+func (mrc *UserController) signIn() {
+
+}
+
+// @Title 登出
+// @Description 用户登出，销毁token
+// @Param	body		body 	models.User	true		"body for user content"
+// @Success 200 {int} models.User.Id
+// @Failure 403 body is empty
+// @router /signOut [post]
+func (mrc *UserController) signOut() {
+
+}
+
+// @Title 注册
+// @Description 用户注册账号
+// @Param	body		body 	models.User	true		"body for user content"
+// @Success 200 {int} models.User.Id
+// @Failure 403 body is empty
+// @router /signUp [post]
+func (mrc *UserController) signUp() {
 
 }
