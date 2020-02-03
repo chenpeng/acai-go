8056c14dd1b7db78daa62696cfbcf15c928e454e
Chen Peng <chenpeng.net@gmail.com>
2020-02-03 22:30:11 +0800

    add AccessToken.go

--- models/AccessToken.go
+++ models/AccessToken.go
@@ -1,7 +1,7 @@
 package models
 
 type AccessToken struct {
-	AccessToken    string
-	RefreshToken   string
-	ExpirationTime int64
+	AccessToken  string
+	RefreshToken string
+	ExpiresIn    int64
 }
