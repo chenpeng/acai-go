5afe4ab3f3e90a2db51b3ab9a5c21ea1764e757f
Chen Peng <chenpeng.net@gmail.com>
2020-02-06 19:00:16 +0800

    add User.go

--- models/User.go
+++ models/User.go
@@ -1,8 +1,32 @@
 package models
 
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+)
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("datasource"))
+	orm.RegisterModel(new(User))
+}
+
 type User struct {
-	username string
-	password string
-	email    string
-	mobile   string
+	Id         int64
+	Username   string
+	Password   string
+	State      int
+	DeleteFlag bool
+}
+
+func FindByUsername(username string) (User, error) {
+	o := orm.NewOrm()
+	user := User{Username: username}
+	err := o.Read(&user, "Username")
+	return user, err
+}
+
+func AddUser(user User) (int64, error) {
+	o := orm.NewOrm()
+	return o.Insert(&user)
 }
