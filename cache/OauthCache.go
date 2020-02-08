86e63cff23cb6299a466de715129adcb271f4b8d
Chen Peng <chenpeng.net@gmail.com>
2020-02-09 00:15:26 +0800

    添加认证相关缓存

--- cache/OauthCache.go
+++ cache/OauthCache.go
@@ -1,5 +1,23 @@
 package cache
 
-func SaveAccessToken(key string) {
+import "acai-go/models"
 
+var oauthCache map[string]models.User
+
+var publicKey = ""
+
+func GetPublicKey() string {
+	return publicKey
+}
+
+func SetPublicKey(key string) {
+	publicKey = key
+}
+
+func GetAccessToken(key string) models.User {
+	return oauthCache[key]
+}
+
+func SetAccessToken(key string, user models.User) {
+	oauthCache[key] = user
 }
